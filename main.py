b57c203ef00928eb05fff66a351e1bc385de7724
pedro <bertuolpedrohenrique@gmail.com>
2025-04-13 17:30:09 -0300

    player moves definitive

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from player import Player
 
 def main():
   pygame.init()
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
   clock = pygame.time.Clock()
   dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,8 +16,9 @@ def main():
       if event.type == pygame.QUIT:
         return
     screen.fill((0, 0, 0))
-    player.update(dt)
-    player.draw(screen)
+    updatable.update(dt)
+    for draw in drawable:
+      draw.draw(screen)
     pygame.display.flip()
     dt = clock.tick(60) / 1000
   print("Starting Asteroids!")
